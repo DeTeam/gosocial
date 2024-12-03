3013abbeb6a32aead0b0b3f7c8407ed69f79c605
Timur Amirov <timur.deteam@gmail.com>
2024-12-03 15:48:42 +0100

    QR code generation

--- serve.go
+++ serve.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 func main() {
+	serverOrigin := "http://localhost:8080"
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -37,8 +42,19 @@ func main() {
 	})
 
 	e.GET("/qr/:invite", func(c echo.Context) error {
-		// TODO: generate a QR code png/jpeg for the given invite token
-		return c.String(http.StatusOK, "TBD")
+		invite := c.Param("invite")
+
+		fullURL := fmt.Sprintf("%s/connect/%s", serverOrigin, invite)
+
+		var png []byte
+		png, err := qrcode.Encode(fullURL, qrcode.Medium, 256)
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "failed to generate QR code")
+		}
+
+		return c.Blob(http.StatusOK, "image/png", png)
+	})
 	})
 
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
